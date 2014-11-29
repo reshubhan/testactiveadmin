8eb35682afc0e2cf2b5382210da9d933250961ea
Amar Singh Kaithwar <reshubhan@gmail.com>
2014-11-29 16:52:33 +0530

    installed mailcatcher

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,15 +23,17 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
+    def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver
+ 
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :city)
+      params.require(:user).permit(:name, :city, :email)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129095817) do
+ActiveRecord::Schema.define(version: 20141129105534) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141129095817) do
     t.text     "city"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
